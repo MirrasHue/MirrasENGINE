4771a8d4448b85523ce6c93db57dd46cc835cec6
MirrasHue <mirralishue@gmail.com>
2024-09-13 17:13:48 -0300

    Added wrapper around backend renderer (OpenGL)

---
+++ MirrasENGINE/src/Core/Renderer/Renderer.cpp
@@ -0,0 +1,53 @@
+#include "Core/Renderer/Renderer.h"
+
+#include "Backends/OpenGL/OpenGLRenderer.h"
+
+namespace mirras
+{
+    OpenGLRenderer Renderer::renderer;
+
+    void Renderer::init(Backend backend)
+    {
+        renderer.init();
+    }
+
+    void Renderer::setViewport(int32 x, int32 y, int32 width, int32 height)
+    {
+        renderer.setViewport(x, y, width, height);
+    }
+
+    void Renderer::clearBackBuffers()
+    {
+        renderer.clearBackBuffers();
+    }
+
+    void Renderer::setClearColor(float r, float g, float b, float a)
+    {
+        renderer.setClearColor(r, g, b, a);
+    }
+
+    void Renderer::beginDrawing()
+    {
+        renderer.beginDrawing();
+    }
+
+    void Renderer::endDrawing()
+    {
+        renderer.endDrawing();
+    }
+
+    void Renderer::beginMode2D(Camera2D& camera)
+    {
+        renderer.beginMode2D(camera);
+    }
+
+    void Renderer::endMode2D()
+    {
+        renderer.endMode2D();
+    }
+
+    void Renderer::drawTriangle()
+    {
+        renderer.drawTriangle();
+    }
+}
---
+++ MirrasENGINE/src/Core/Renderer/Renderer.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <memory>
+
+#include "Core/Utils.h"
+
+namespace mirras
+{
+    class Camera2D;
+
+    class Renderer
+    {
+    public:
+        enum class Backend : uint8
+        {
+            OpenGL
+        };
+
+    public:
+        Renderer() = delete;
+
+        static void init(Backend backend);
+        static void setViewport(int32 x, int32 y, int32 width, int32 height);
+        
+        static void clearBackBuffers();
+        static void setClearColor(float r, float g, float b, float a = 1.f);
+
+        static void beginDrawing();
+        static void endDrawing();
+
+        static void beginMode2D(Camera2D& camera);
+        static void endMode2D();
+
+        static void drawTriangle();
+
+    private:
+        // In the eventual support of another graphics API, we'd have an abstract class 'BackendRenderer' that declares all
+        // the methods we find in OpenGLRenderer. And all renderers would inherit from that class and conform to its interface.
+        // We would also need to abstract context creation, given that the default one provided by GLFW is meant for OpenGL
+        static class OpenGLRenderer renderer;
+        // This would become something like 'static std::unique_ptr<class BackendRenderer> renderer', and in the 'init'
+        // method we'd switch on the backend selected by the client and instantiate an actual renderer based on that
+    };
+}
