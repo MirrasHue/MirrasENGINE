ebc45c7f91a92f06164d0c03efb56e60e56619e3
MirrasHue <mirralishue@gmail.com>
2024-09-13 17:12:09 -0300

    Added 2D camera with some functionalities

---
+++ MirrasENGINE/src/Core/Renderer/Camera2D.cpp
@@ -0,0 +1,49 @@
+#include "Core/Renderer/Camera2D.h"
+
+#include "Core/Application.h"
+
+namespace mirras
+{
+    Camera2D::Camera2D(vec2f _position, vec2f _offset, float _rotation, float _zoom)
+        : position{_position}, rotation{_rotation}, zoom{_zoom}
+    {
+        setOffset(_offset.x, _offset.y);
+    }
+
+    void Camera2D::setOffset(float xOffset, float yOffset)
+    {
+        auto [currentWidth, currentHeight] = App::getOSWindow().getFramebufferSize();
+        static auto [initialWidth, initialHeight] = App::getOSWindow().getInitialFbSize();
+
+        offsetRatio.x = xOffset / initialWidth;
+        offsetRatio.y = yOffset / initialHeight;
+
+        offset.x = offsetRatio.x * currentWidth;
+        offset.y = offsetRatio.y * currentHeight;
+    }
+
+    void Camera2D::setOffsetX(float xOffset)
+    {
+        setOffset(xOffset, offset.y);
+    }
+
+    void Camera2D::setOffsetY(float yOffset)
+    {
+        setOffset(offset.x, yOffset);
+    }
+
+    void Camera2D::onWindowResize(Event& event)
+    {
+        if(!Event::is_a<WindowResized>(event))
+            return;
+
+        auto[currentWidth, currentHeight] = static_cast<WindowResized&>(event).windowSize;
+
+        static int32 initialFbHeight = App::getOSWindow().getInitialFbSize().y;
+
+        zoom = (float)currentHeight / initialFbHeight;
+
+        offset.x = offsetRatio.x * currentWidth;
+        offset.y = offsetRatio.y * currentHeight;
+    }
+}
---
+++ MirrasENGINE/src/Core/Renderer/Camera2D.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Core/Utils.h"
+
+namespace mirras
+{
+    class Camera2D
+    {
+    public:
+        Camera2D() = default;
+        Camera2D(vec2f position, vec2f offset = {}, float rotation = 0.f, float zoom = 1.f);
+
+        void setOffset(float xOffset, float yOffset);
+        void setOffsetX(float xOffset);
+        void setOffsetY(float yOffset);
+
+        vec2f getOffset() { return offset; }
+
+        void onWindowResize(class Event& event);
+
+    public:
+        vec2f position; // Aligns with the center of the screen (when the offset is 0,0)
+        
+        float rotation = 0.f; // In degrees
+        float zoom = 1.f;
+
+    private:
+        // The ratios need to be recalculated when the offset changes, so rendering scales correctly when the window resizes,
+        vec2f offset; // that's why offset is not accessed directly
+        vec2f offsetRatio; // With respect to the initial window size
+    };
+}
